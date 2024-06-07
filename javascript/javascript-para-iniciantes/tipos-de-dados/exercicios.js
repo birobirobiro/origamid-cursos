4916a8cd97337ac57872f6fd192638ee2a1bcda3
João Inácio Neto <birobirobiro.dev@gmail.com>
2024-06-06 21:10:32 -0300

    tipos de dados

---
+++ javascript/javascript-para-iniciantes/tipos-de-dados/exercicios.js
@@ -0,0 +1,31 @@
+// Declare uma variável contendo uma string
+
+var teste = "testando";
+console.log(teste);
+
+// Declare uma variável contendo um número dentro de uma string
+
+var teste2 = "Esse é o número 2";
+console.log(teste2);
+
+// Declare uma variável com a sua idade
+
+var idade = 31;
+console.log(idade);
+
+// Declare duas variáveis, uma com seu nome e outra com seu sobrenome e some as mesmas
+
+var nome = "João";
+var sobrenome = "Inácio";
+var nomeCompleto = `${nome} ${sobrenome}`;
+console.log(nomeCompleto);
+
+// Coloque a seguinte frase em uma variável I'ts time
+
+var frase = `It's time`;
+console.log(frase);
+
+// Verifique o tipo de variável que contém o seu nome
+var verificarTipoNome = typeof nome;
+
+console.log(verificarTipoNome);
