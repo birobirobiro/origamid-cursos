1915017f775e51aca83cc14a0ba791d6e9bb9e7c
João Inácio Neto <birobirobiro.dev@gmail.com>
2024-06-06 21:10:21 -0300

    Variáveis

---
+++ javascript/javascript-para-iniciantes/variaveis/exercicios.js
@@ -0,0 +1,30 @@
+// EXERCÍCIOS
+
+// Declarar uma variável com o seu nome
+
+var nome = "João Inácio Neto";
+console.log(nome);
+
+// Declarar uma variável com a sua idade
+var idade = "31";
+console.log(idade);
+
+// Declarar uma variável com a sua comida favorita e não atribuir valor
+
+var comidaFavorita;
+console.log(comidaFavorita);
+
+// Atribuir valor a sua comida favorita
+
+comidaFavorita = "macarrão";
+console.log(comidaFavorita);
+
+// Declarar 5 variáveis diferentes sem valores
+
+const gatos = "Bacon, Sushi, Docinho e Coxinha";
+
+console.log(gatos);
+
+var time, professor, pais, endereco, rua;
+
+console.log(time, professor, pais, endereco, rua);
