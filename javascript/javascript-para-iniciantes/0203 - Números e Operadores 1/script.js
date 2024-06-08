b680366648fb38ef590d09a30d0d147446998cec
João Inácio Neto <birobirobiro.dev@gmail.com>
2024-06-08 10:52:03 -0300

    Números e Operadores 1

---
+++ javascript/javascript-para-iniciantes/0203 - Números e Operadores 1/script.js
@@ -0,0 +1,20 @@
+var idade = 31e-10;
+console.log(idade);
+
+var soma = 100 + 50;
+var subtracao = 100 - 50;
+var multiplicacao = 100 * 2;
+var divisao = 100 / 2;
+var expoente = 2 ** 4;
+var modulo = 14 % 5;
+
+console.log(soma, subtracao, multiplicacao, divisao, expoente, modulo);
+
+// Operadores Aritméticos (Strings)
+
+var soma = "100" + 50;
+var subtracao = "100" - 50;
+var multiplicacao = "100" * "2";
+var divisao = "Comprei 10" / 2;
+
+console.log(soma, subtracao, multiplicacao, divisao);
